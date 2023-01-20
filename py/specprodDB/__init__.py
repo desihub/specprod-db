56a3ff3aac4d08b4222b4de98005dc3522a7954b
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2023-01-20 16:25:19 -0700

    add python components

---
+++ py/specprodDB/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+specprodDB
+==========
+
+Tools for loading DESI data into databases.
+
+In particular, this code loads the spectroscopic production database.
+
+It does *not* include:
+
+#. Pipeline processing status database.
+#. Imaging and targeting databases.
+"""
