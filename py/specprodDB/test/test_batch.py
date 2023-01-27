96ba74062796b6bc11da9cc864a0058b6e76ed06
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2023-01-26 17:00:05 -0700

    placeholder

---
+++ py/specprodDB/test/test_batch.py
@@ -0,0 +1,25 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test specprodDB.batch.
+"""
+import unittest
+from unittest.mock import patch
+
+
+class TestBatch(unittest.TestCase):
+    """Test specprodDB.batch.
+    """
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
