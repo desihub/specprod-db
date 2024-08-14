e6eef4d0226cdd1d9300a8ca04d147f2999ec792
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2024-08-14 12:46:45 -0700

    update container startup script

--- spin/db/00-setup-desi.sh
+++ spin/db/00-setup-desi.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-set -o errexit;
+set -o errexit
 
 [[ -n "${DESI_ADMIN_PASSWORD}" && -n "${DESI_PASSWORD}" ]]
 
+if [[ -f "${DESI_ADMIN_PASSWORD}" ]]; then
+    desi_admin=$(<${DESI_ADMIN_PASSWORD})
+else
+    desi_admin="${DESI_ADMIN_PASSWORD}"
+fi
+
+if [[ -f "${DESI_PASSWORD}" ]]; then
+    desi=$(<${DESI_PASSWORD})
+else
+    desi="${DESI_PASSWORD}"
+fi
+
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
-    CREATE USER desi_admin WITH CREATEDB ENCRYPTED PASSWORD '$(<${DESI_ADMIN_PASSWORD})';
-    CREATE USER desi WITH ENCRYPTED PASSWORD '$(<${DESI_PASSWORD})';
+    CREATE USER desi_admin WITH CREATEDB ENCRYPTED PASSWORD '${desi_admin}';
+    CREATE USER desi WITH ENCRYPTED PASSWORD '${desi}';
     CREATE DATABASE desi WITH OWNER desi_admin;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname desi -c 'CREATE EXTENSION q3c;'
-
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname desi <<-EOSQLDESI
+    CREATE EXTENSION q3c;
+    ALTER SCHEMA public OWNER TO desi_admin;
+EOSQLDESI
