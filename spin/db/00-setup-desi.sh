f5de544f575361d56b64ea1ae7528ea4eb975129
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2024-09-25 10:52:19 -0700

    add support for desi_public

--- spin/db/00-setup-desi.sh
+++ spin/db/00-setup-desi.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -o errexit
 
-[[ -n "${DESI_ADMIN_PASSWORD}" && -n "${DESI_PASSWORD}" ]]
+[[ -n "${DESI_ADMIN_PASSWORD}" && \
+   -n "${DESI_PASSWORD}" && \
+   -n "${DESI_PUBLIC_PASSWORD}" ]]
 
 if [[ -f "${DESI_ADMIN_PASSWORD}" ]]; then
     desi_admin=$(<${DESI_ADMIN_PASSWORD})
@@ -15,9 +17,16 @@ else
     desi="${DESI_PASSWORD}"
 fi
 
+if [[ -f "${DESI_PUBLIC_PASSWORD}" ]]; then
+    desi_public=$(<${DESI_PUBLIC_PASSWORD})
+else
+    desi_public="${DESI_PUBLIC_PASSWORD}"
+fi
+
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
     CREATE USER desi_admin WITH CREATEDB ENCRYPTED PASSWORD '${desi_admin}';
     CREATE USER desi WITH ENCRYPTED PASSWORD '${desi}';
+    CREATE USER desi_public WITH ENCRYPTED PASSWORD '${desi_public}';
     CREATE DATABASE desi WITH OWNER desi_admin;
 EOSQL
 
