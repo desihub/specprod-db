91f8b4834dfd92a2c747fc849e477bd5c00a601f
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-10-06 14:20:31 -0700

    add tests for copy_columns

--- py/specprodDB/test/test_coeff.py
+++ py/specprodDB/test/test_coeff.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 """Test specprodDB.coeff.
 """
-import os
+# import os
 import unittest
-from unittest.mock import patch, mock_open, call
-from ..coeff import get_options
+from unittest.mock import patch, MagicMock, call
+import numpy as np
+from astropy.table import Table
+from ..coeff import get_options, copy_columns
+
 
 class TestCoeff(unittest.TestCase):
     """Test specprodDB.coeff.
@@ -31,3 +34,69 @@ class TestCoeff(unittest.TestCase):
         options = get_options()
         self.assertTrue(options.verbose)
         self.assertEqual(options.zall, 'zall-pix-iron.fits')
+
+    def test_copy_columns_zpix(self):
+        """Test copy_columns() for a zpix table.
+        """
+        def rowfilter(catalog):
+            return np.array([0, 1, 3])
+        w = rowfilter('foo')
+        catalog = Table()
+        catalog['TARGETID'] = np.array([1, 2, 3, 4])
+        catalog['SURVEY'] = np.array(['main']*4)
+        catalog['PROGRAM'] = np.array(['dark']*4)
+        catalog['HEALPIX'] = np.array([137]*4)
+        catalog['DESINAME'] = np.array(['name']*4)
+        # catalog['TILEID'] = np.array([314]*4)
+        # catalog['SPGRPVAL'] = np.array([20250703]*4)
+        # catalog['TARGET_RA'] = np.random.uniform(size=(4,))
+        # catalog['TARGET_DEC'] = np.random.uniform(size=(4,))
+        catalog['COEFF'] = np.random.normal(size=(4, 10))
+        patch_table = copy_columns(catalog, rowfilter, 'pix')
+        self.assertEqual(patch_table.meta['EXTNAME'], 'COEFF_PATCH')
+        self.assertTrue((patch_table['TARGETID'] == np.array([1, 2, 4])).all())
+        self.assertTrue((patch_table['COEFF_5'] == catalog['COEFF'][w, 5]).all())
+
+    def test_copy_columns_ztile(self):
+        """Test copy_columns() for a ztile table.
+        """
+        def rowfilter(catalog):
+            return np.array([0, 1, 3])
+        w = rowfilter('foo')
+        catalog = Table()
+        catalog['TARGETID'] = np.array([1, 2, 3, 4])
+        # catalog['SURVEY'] = np.array(['main']*4)
+        # catalog['PROGRAM'] = np.array(['dark']*4)
+        # catalog['HEALPIX'] = np.array([137]*4)
+        catalog['DESINAME'] = np.array(['name']*4)
+        catalog['TILEID'] = np.array([314]*4)
+        catalog['SPGRPVAL'] = np.array([20250703]*4)
+        # catalog['TARGET_RA'] = np.random.uniform(size=(4,))
+        # catalog['TARGET_DEC'] = np.random.uniform(size=(4,))
+        catalog['COEFF'] = np.random.normal(size=(4, 10))
+        patch_table = copy_columns(catalog, rowfilter, 'tilecumulative')
+        self.assertEqual(patch_table.meta['EXTNAME'], 'COEFF_PATCH')
+        self.assertTrue((patch_table['TARGETID'] == np.array([1, 2, 4])).all())
+        self.assertTrue((patch_table['COEFF_5'] == catalog['COEFF'][w, 5]).all())
+
+    def test_copy_columns_no_desiname(self):
+        """Test copy_columns() for a zpix table without DESINAME.
+        """
+        def rowfilter(catalog):
+            return np.array([0, 1, 3])
+        w = rowfilter('foo')
+        catalog = Table()
+        catalog['TARGETID'] = np.array([1, 2, 3, 4])
+        catalog['SURVEY'] = np.array(['main']*4)
+        catalog['PROGRAM'] = np.array(['dark']*4)
+        catalog['HEALPIX'] = np.array([137]*4)
+        # catalog['DESINAME'] = np.array(['name']*4)
+        # catalog['TILEID'] = np.array([314]*4)
+        # catalog['SPGRPVAL'] = np.array([20250703]*4)
+        catalog['TARGET_RA'] = np.random.uniform(size=(4,))
+        catalog['TARGET_DEC'] = np.random.uniform(size=(4,))
+        catalog['COEFF'] = np.random.normal(size=(4, 10))
+        patch_table = copy_columns(catalog, rowfilter, 'pix')
+        self.assertEqual(patch_table.meta['EXTNAME'], 'COEFF_PATCH')
+        self.assertTrue((patch_table['TARGETID'] == np.array([1, 2, 4])).all())
+        self.assertTrue((patch_table['COEFF_5'] == catalog['COEFF'][w, 5]).all())
