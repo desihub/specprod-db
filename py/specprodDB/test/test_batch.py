32260a9a7a6acb18336612e21b6e2b38018f5d5b
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2026-01-22 15:22:44 -0700

    minor tweak to test config

--- py/specprodDB/test/test_batch.py
+++ py/specprodDB/test/test_batch.py
@@ -16,6 +16,7 @@ class TestBatch(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.maxDiff = None
+        cls.thirteen = sys.version_info.major == 3 and sys.version_info.minor >= 13
 
     @classmethod
     def tearDownClass(cls):
@@ -159,13 +160,13 @@ exit ${{load_status}}
                                call().__enter__(),
                                call().write('abcd'),
                                call().__exit__(None, None, None)]
-        if sys.version_info.major == 3 and sys.version_info.minor >= 13:
+        if self.thirteen:
             write_scripts_calls += [call().close()]
         write_scripts_calls += [call(os.path.join(os.environ['HOME'], 'Documents', 'Jobs', 'bar.sh'), 'w'),
                                 call().__enter__(),
                                 call().write('abcd'),
                                 call().__exit__(None, None, None)]
-        if sys.version_info.major == 3 and sys.version_info.minor >= 13:
+        if self.thirteen:
             write_scripts_calls += [call().close()]
         m.assert_has_calls(write_scripts_calls)
         handle = m()
